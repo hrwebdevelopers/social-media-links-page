d0744e4e5e1b587fe2dc8309ae4ad33a2c9df88c
Hector Romero <hectorromerodev@gmail.com>
2022-10-22 15:07:40 -0700

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <ShareComponent />
+  <!-- <HeaderComponent /> -->
+  <img alt="App logo" src="./assets/logo.png" height="60">
+  <!-- <ProfilePage /> -->
+  <SocialMediaLinks title="@hrwebdevelopers" description="We build blazing-fast, beautiful websites for startups, small businesses and enterprises."/>
+  <!-- <FooterComponent /> -->
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SocialMediaLinks from './components/SocialMediaLinks.vue';
+  import ShareComponent from './components/shareComponent.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      SocialMediaLinks,
+      ShareComponent
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    background: rgb(242,157,122);
+    background: linear-gradient(0deg, rgba(242,157,122,1) 0%, rgba(28,65,171,1) 100%);
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #f1f1f1;
+    margin-top: 60px;
+  }
 </style>
