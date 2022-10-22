2c6200c3324a2390791be566715643f405497aed
Hector Romero <hectorromerodev@gmail.com>
2022-10-22 15:07:48 -0700

    add icons and config to main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faFacebook, faInstagram, faWhatsapp, faGithub, faLinkedin, faTwitter, faFacebookMessenger } from '@fortawesome/free-brands-svg-icons'
+import { faChevronRight, faCopy, faEnvelope, faShareAlt, faX } from '@fortawesome/free-solid-svg-icons';
+
+
+/* add icons to the library */
+library.add(faFacebook,
+  faInstagram,
+  faWhatsapp,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faFacebookMessenger,
+  faEnvelope,
+  faShareAlt,
+  faX,
+  faChevronRight,
+  faCopy
+)
+
+createApp(App)
+  .component('fa-icon', FontAwesomeIcon)
+  .mount('#app')
