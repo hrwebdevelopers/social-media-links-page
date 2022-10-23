0358873040ce56c769f2bc5c0e5d47c12a7f6eea
Hector Romero <hectorromerodev@gmail.com>
2022-10-22 20:53:21 -0700

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
+  <ThemeToggleComponent />
 
   <ShareComponent />
   <!-- <HeaderComponent /> -->
-  <img alt="App logo" src="./assets/logo.png" height="60">
+  <img alt="App logo" src="/logo.png" height="60" />
   <!-- <ProfilePage /> -->
   <SocialMediaLinks title="@hrwebdevelopers" description="We build blazing-fast, beautiful websites for startups, small businesses and enterprises."/>
   <!-- <FooterComponent /> -->
@@ -11,34 +12,52 @@
 
 <script>
   import SocialMediaLinks from './components/SocialMediaLinks.vue';
-  import ShareComponent from './components/shareComponent.vue';
+  import ShareComponent from './components/ShareComponent.vue';
+  import ThemeToggleComponent from './components/ThemeToggleComponent.vue';
 
   export default {
     name: 'App',
     components: {
       SocialMediaLinks,
-      ShareComponent
+      ShareComponent,
+      ThemeToggleComponent
     }
   }
 </script>
 
 <style>
-  html {
-    background: rgb(242,157,122);
-    background: linear-gradient(0deg, rgba(242,157,122,1) 0%, rgba(28,65,171,1) 100%);
-    height: 100vh;
+#app {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: #f1f1f1;
+    margin-top: 60px;
+  }
+
+html {
+  height: 100vh;
     width: 100vw;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #f1f1f1;
-    margin-top: 60px;
+    background-color: #f1f1f1;
+      color: #2c3e50;
+    }
+    
+    a {
+      color: #2c3e50;
+      text-decoration: none;
+      transition: all 0.3s ease;
+    }
+    
+    .dark {
+      background-color: #2c3e50;
+      color: #f1f1f1;
+    }
+    
+    fa-icon {
+      color: #2c3e50
   }
 </style>
