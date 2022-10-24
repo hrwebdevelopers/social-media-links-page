79f6006cee9689583485324607c9ac7923f4664d
Hector Romero <hectorromerodev@gmail.com>
2022-10-24 16:58:54 -0700

    add colors variables .css

--- src/App.vue
+++ src/App.vue
@@ -26,24 +26,7 @@ export default {
 </script>
 
 <style>
-:root {
-  --primary-color: #1c41ab;
-  --secondary-color: #305dfc;
-  --accent-color: #f7f7f7;
-  --font-color: #424242;
-  --bg-color: #fff;
-  --heading-color: #292922;
-
-  /* #2c3e50 */
-}
-
-[data-theme="dark"] {
-  --primary-color: #9A97F3;
-  --secondary-color: #818cab;
-  --font-color: #e1e1ff;
-  --bg-color: #161625;
-  --heading-color: #818cab;
-}
+@import '../public/css/colors.css ';
 
 
 
@@ -53,11 +36,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: var(--font-color);
-    background-color: var(--bg-color);
-    margin-top: 60px;
-  }
+  background-color: var(--bg-color);
+  margin-top: 60px;
+}
 
-  html {
+html {
     height: 100vh;
     width: 100vw;
     margin: 0;
